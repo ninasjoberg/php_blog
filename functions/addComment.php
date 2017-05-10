f33ede75a5b080f086020316f20fbcfff8f3d249
ninasjoberg <nina.sjoberg@gmail.com>
2017-05-10 23:12:40 +0200

    errorhandling and fetch usage started, not done

--- functions/addComment.php
+++ functions/addComment.php
@@ -3,11 +3,34 @@
 	include '../error.php';
 	include '../classes/posts.php';
 
-	$pdo = Database::connection();
-	$db = new Posts($pdo);
+		$errorMessage = '';
+		$response = array();
 
-    $id = $_POST['commentedPostId'];
-    $text = $_POST['comment'];
-	$name = $_POST['name'];
-    $db->insertComment($text, $id, $name);
+		$id = $_POST['commentedPostId'];
+		$name = trim($_POST['name']);
+		$text = trim($_POST['comment']);
+
+		if($name == ""){
+			$errorMessage .= 'You must enter your name. ';
+			echo json_encode(array('status' => 400, 'message' => $errorMessage));
+		}
+
+		if($text == ''){
+			$errorMessage .= 'You must enter a comment. ';
+			echo json_encode(array('status' => 400, 'message' => $errorMessage));
+		}
+
+		if($errorMessage == ''){
+			$pdo = Database::connection();
+			$db = new Posts($pdo);
+			$db->insertComment($text, $id, $name);
+			echo json_encode(array('status' => 200, 'text' => $text, 'name' => $name, 'created' => date("Y-m-d h:i:sa")));
+		}
+		
+		else {
+			$errorMessage = 'something went wront';
+			$response = array('status' => 500, 'message' => $errorMessage);
+			echo json_encode($response);
+		}
 ?>
+
--- single.php
+++ single.php
@@ -3,12 +3,15 @@ include ("partials/header.php");
 include 'functions/showAllComments.php';
 include_once dirname(__FILE__) . '/classes/posts.php'; 
 
+
 if(isset($_GET['post'])) {
   $id = $_GET['post'];
   $pdo = Database::connection();
   $db = new Posts($pdo);
   $blogPost = $db->getById('posts', $id);
 }
+
+var_dump($_GET);
 ?>
 
 <br>
@@ -24,11 +27,11 @@ if(isset($_GET['post'])) {
 <h3>Comment</h3>
 <form action="functions/addComment.php" method="POST" id="comment-form">
   <label>Namn</label>
-  <input type="text" name="name" class="form-control" id="exampleInputEmail1" placeholder="Name (Optional)">
+  <input type="text" name="name" class="form-control" placeholder="Name (Optional)">
 	<label>Comment</label>
   <textarea name="comment" rows="20" cols="60" class="form-control"></textarea><br>
 	<input type="hidden" value="<?php echo $row['id']?>" name="commentedPostId">
-	<input type="submit" value="post" class="btn btn-primary">
+	<input type="submit" value="post" name='submit' class="btn btn-primary">
 </form>
 <a href="index.php" class="btn btn-primary">Back</a>
 
@@ -36,18 +39,23 @@ if(isset($_GET['post'])) {
 
 
 <h1>Previous comments: </h1>
-<ul>
-    <?php foreach ($commentList as $row) { 
-      if ($row["postId"] == $id) {?>
+<ul id="comment-list">
+  <?php foreach ($commentList as $row) { 
+    if ($row["postId"] == $id) {?>
+      <li>
         <p> <?php echo $row["text"] ?> </p>
         <p> Comment by: <?php echo $row["name"] ?> </p>
         <p> Created: <?php echo $row["created"] ?> </p>
-        <hr>
-    <?php } }?>
+      </li>
+      <hr>
+  <?php } }?>
 </ul>
 
 
-<script> <?php include '../js/fetch.js' ?> </script>
+<script>
+    <?php
+        include 'js/fetch.js';
+    ?>
+</script>
 
-<?php include ("partials/footer.php"); ?>
 
