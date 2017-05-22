7059fd33220dc53eb3033798fea7ecb5ade5c7e7
nicfue <nicolasfuentes@hotmail.com>
2017-05-22 11:45:59 +0200

    db.php

--- db.php
+++ db.php
@@ -3,9 +3,9 @@
 class Database{
   public static function connection() {
   return new PDO(
-    "mysql:host=localhost;dbname=nmnblog;charset=utf8",
+    "mysql:host=localhost;dbname=nmnBlog;charset=utf8",
     "root",
-    "ekonomen76", [ 
+    "ekonomen76", [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
       PDO::ATTR_EMULATE_PREPARES   => false
